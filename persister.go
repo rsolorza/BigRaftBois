d85338da6ca278b31aab9075059ea19119664737
Ryan Solorzano <rsolorza@calpoly.edu>
2020-10-23 19:43:06 -0700

    Add half of persister and make changes to raft.go

--- persister.go
+++ persister.go
@@ -1,70 +1,104 @@
 package main
 
-//
-// support for Raft and kvraft to save persistent
-// Raft state (log &c) and k/v server snapshots.
-//
-// we will use the original persister.go to test your code for grading.
-// so, while you can modify this code to help you debug, please
-// test with the original before submitting.
-//
+type Log struct {
+    term      int
+    idx       int
+    committed bool
+    newState  StateMachine
+}
 
-import "sync"
+type StateMachine struct {
+    x int
+    y int 
+    z int
+}
 
 type Persister struct {
-	mu        sync.Mutex
-	raftstate []byte
-	snapshot  []byte
+	currentTerm int
+	votedFor    int
+	logs        []Log
+}
+
+type Follower struct {
+	commitIndex int
+	lastApplied int
+}
+
+type Leader struct {
+	nextIndex []int
+	matchIndex []int
 }
 
 func MakePersister() *Persister {
-	return &Persister{}
+	return &Persister{-1, -1, nil}
 }
 
-func (ps *Persister) Copy() *Persister {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	np := MakePersister()
-	np.raftstate = ps.raftstate
-	np.snapshot = ps.snapshot
-	return np
+func (ps *Persister) MakeLeader(me *Follower, id int) *Leader {
+	leader := Leader{}
+	leader.nextIndex = make([]int, numNodes)
+	leader.matchIndex = make([]int, numNodes)
+
+	for i := 0; i < id; i++ {
+		if i != id {
+			leader.nextIndex[i] = me.commitIndex + 1
+			leader.matchIndex[i] = 0
+		}
+	}
+
+	return &leader
 }
 
-func (ps *Persister) SaveRaftState(state []byte) {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	ps.raftstate = state
+// Can be used as initialization or recovery
+func (ps *Persister) MakeFollower() *Follower {
+	if (ps.logs == nil || ps.currentTerm == -1) {
+		ps.currentTerm = 0
+		ps.logs = make([]Log, 1)
+
+		ps.logs[0] = Log{0, 0, false, StateMachine{0,0,0}}
+
+		return &Follower{0, 0}
+	}
+
+	for i := len(ps.logs); i > 0; i-- {
+		if (ps.logs[i].committed) {
+			return &Follower{i, len(ps.logs)}
+		}
+	}
+
+	return &Follower{0, 0}
 }
 
-func (ps *Persister) ReadRaftState() []byte {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	return ps.raftstate
+// returns true if able to add to log, false otherwise
+func (ps *Persister) FollowerRecieveLog(me *Follower, log Log, prev Log) bool {
+	if prev.idx == ps.logs[me.lastApplied].idx && prev.term == ps.logs[me.lastApplied].idx {
+		ps.logs = append(ps.logs, log)
+		ps.logs[me.lastApplied].committed = true
+		
+		me.commitIndex++
+		me.lastApplied++
+
+		return true
+	}
+
+	return false
 }
 
-func (ps *Persister) RaftStateSize() int {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	return len(ps.raftstate)
+// Assumes new log to be sent has been added, but not committed locally
+func (ps *Persister) SendLogs(me *Leader, peers []chan Message, id int) {
+	for i := 0; i < numNodes; i++ {
+		if (i != id) {
+			logIdx := me.nextIndex[i]
+
+			peers[i] <- Message{LOG, id, ps.logs[logIdx], ps.logs[logIdx - 1]}
+		}
+	}
 }
 
-// Save both Raft state and K/V snapshot as a single atomic action,
-// to help avoid them getting out of sync.
-func (ps *Persister) SaveStateAndSnapshot(state []byte, snapshot []byte) {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	ps.raftstate = state
-	ps.snapshot = snapshot
+func (ps *Persister) LeaderRecieveLogAck(me *Leader, msg Message) {
+	
 }
 
-func (ps *Persister) ReadSnapshot() []byte {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	return ps.snapshot
+func (ps *Persister) UpdateCommitted() {
+
 }
 
-func (ps *Persister) SnapshotSize() int {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	return len(ps.snapshot)
-}
--- raft.go
+++ raft.go
@@ -1,191 +1,218 @@
 package main
 
 import (
-	"fmt"
-//	"sync"
-	"math/rand"
-	"time"
+    "fmt"
+    "math/rand"
+    "time"
 )
 
 const (
-	numNodes 				int = 8 //num of raft nodes
-	numTerms				int = 10 //num of terms to loop through
-	maxChanBuff 		int = 10000 //max size of buffer
-	minTimeOut 			int = 150	//min timeout for trying to become leader
-	maxTimeOut 			int = 300 //max timeout for trying to become leader
-	voteTimeOut 		int64 = 100 //timeout for waiting for requested votes
-	voteSentTimeOut int64 = 250 //timeout waiting for vote to result in leader
-	termDuration 		int64 = 2000 //duration of term for leader for debugging
-	leaderTimeOut 	int64 = 150 //timeout for needed hb's from leader to followe
-	//types of messages
-	VOTE		int = 0 //request for a vote
-	ACK			int = 1	//ack for vote
-	HB			int = 2	//heartbeat of the leader
-	LEADER	int = 3	//request to see if leader
+    numNodes            int = 8 //num of raft nodes
+    numTerms            int = 10 //num of terms to loop through
+    maxChanBuff         int = 10000 //max size of buffer
+    minTimeOut          int = 150   //min timeout for trying to become leader
+    maxTimeOut          int = 300 //max timeout for trying to become leader
+    voteTimeOut         int64 = 100 //timeout for waiting for requested votes
+    voteSentTimeOut     int64 = 250 //timeout waiting for vote to result in leader
+    termDuration        int64 = 2000 //duration of term for leader for debugging
+    leaderTimeOut       int64 = 150 //timeout for needed hb's from leader to followe
+    
+    //types of messages
+    VOTE                int = 0 //request for a vote
+    ACK                 int = 1 //ack for vote
+    HB                  int = 2 //heartbeat of the leader
+    LEADER              int = 3 //request to see if leader
+    LOG                 int = 4 //leader sending out log to commit
+    LOG_ACK             int = 5 //follower ack'ing commit to log
 )
 
 type Message struct {
-	messageType int
-	sender			int
+    messageType int
+    sender      int
+    newLog      Log
+    prevLog     Log
 }
 
+
 func ElectLeader(peers []chan Message, id int, persister *Persister) bool {
-	leaderSearching := true
-	isLeader := false
-	sentVote := false
-	lastReset := time.Now()
-	lastVote := time.Now()
-	timeOut := int64(rand.Intn(maxTimeOut- minTimeOut) + minTimeOut)
+    leaderSearching := true
+    isLeader := false
+    sentVote := false
+    lastReset := time.Now()
+    lastVote := time.Now()
+    timeOut := int64(rand.Intn(maxTimeOut- minTimeOut) + minTimeOut)
 
-	for leaderSearching { 
-		if time.Since(lastReset).Milliseconds() < timeOut { //read messages in channel to see if others are requesting votes or are already the leader
-			keepReading := true
-			for keepReading {
-				select {
-					case message := <- peers[id]:
-						if message.messageType == VOTE {
-							if !sentVote {
-								sentVote = true
-								peers[message.sender] <- Message{ACK, id}
-								lastVote = time.Now()
-								lastReset = time.Now()
-							}
-						} else if message.messageType == HB {
-							keepReading = false
-							leaderSearching = false
-						}
-						if time.Since(lastVote).Milliseconds() > voteSentTimeOut {
-							sentVote = false
-						}
-					default:
-						keepReading = false
-				}
-			}
-		} else { //try to become the leader by gaining the majority of votes
-			for i := 0; i < numNodes; i++ {
-				if i != id {
-					peers[i] <- Message{VOTE, id}
-				}
-			}
-			// need to gain majority before the timeout for voting occurs
-			needed := numNodes / 2 + 1 //majority
-			needed-- //vote for self
-			voteStart := time.Now()
-			for time.Since(voteStart).Milliseconds() < voteTimeOut {
-				select {
-					case message := <- peers[id]:
-						if message.messageType == ACK {
-							needed--
-						} else if message.messageType == HB{ //another leader was already elected so can't be elected
-							needed = numNodes
-						}
-					default:
-						time.Sleep(10 * 1e6)
-				}
-			}
-			if needed <= 0 { //elected
-				leaderSearching = false
-				isLeader = true
-			} else { //not elected
-				lastReset = time.Now()
-				timeOut = int64(rand.Intn(maxTimeOut- minTimeOut) + minTimeOut)
+    for leaderSearching { 
+        if time.Since(lastReset).Milliseconds() < timeOut { //read messages in channel to see if others are requesting votes or are already the leader
+            keepReading := true
+            for keepReading {
+                select {
+                    case message := <- peers[id]:
+                        if message.messageType == VOTE {
+                            if !sentVote {
+                                sentVote = true
+                                peers[message.sender] <- Message{ACK, id, Log{}, Log{}}
+                                lastVote = time.Now()
+                                lastReset = time.Now()
+                            }
+                        } else if message.messageType == HB {
+                            keepReading = false
+                            leaderSearching = false
+                        }
+                        if time.Since(lastVote).Milliseconds() > voteSentTimeOut {
+                            sentVote = false
+                        }
+                    default:
+                        keepReading = false
+                }
+            }
+        } else { //try to become the leader by gaining the majority of votes
+            for i := 0; i < numNodes; i++ {
+                if i != id {
+                    peers[i] <- Message{VOTE, id, Log{}, Log{}}
+                }
+            }
+            // need to gain majority before the timeout for voting occurs
+            needed := numNodes / 2 + 1 //majority
+            needed-- //vote for self
+            voteStart := time.Now()
+            
+            for time.Since(voteStart).Milliseconds() < voteTimeOut {
+                select {
+                    case message := <- peers[id]:
+                        if message.messageType == ACK {
+                            needed--
+                        } else if message.messageType == HB{ //another leader was already elected so can't be elected
+                            needed = numNodes
+                        }
+                    default:
+                        time.Sleep(10 * 1e6)
+                }
+            }
+            
+            if needed <= 0 { //elected
+                leaderSearching = false
+                isLeader = true
+            } else { //not elected
+                lastReset = time.Now()
+                timeOut = int64(rand.Intn(maxTimeOut- minTimeOut) + minTimeOut)
 
-			}
-		}
-		time.Sleep(10 * 1e6)
-	}
-	return isLeader
+            }
+        }
+        time.Sleep(10 * 1e6)
+    }
+    
+    return isLeader
 }
 
 func RunLeader(peers []chan Message, id int, persister *Persister, applyChan chan Message) {
-	termStart := time.Now()
-	for time.Since(termStart).Milliseconds() < termDuration {
-		for i := 0; i < numNodes; i++ {
-			if i != id {
-				peers[i] <- Message{HB, id}
-			}
-		}
-		select{
-			case message := <- peers[id]:
-				if message.messageType == LEADER {
-					applyChan <- Message{ACK, id}
-				}
-		}
-		time.Sleep(1e6 * 100)
-	}
-	time.Sleep(1e6 * 100)
+    termStart := time.Now()
+    for time.Since(termStart).Milliseconds() < termDuration {
+        for i := 0; i < numNodes; i++ {
+            if i != id {
+                peers[i] <- Message{HB, id, Log{}, Log{}}
+            }
+        }
+
+        chanIsEmpty := false
+        for !chanIsEmpty {
+            select {
+                case message := <- peers[id]:
+                    if message.messageType == LEADER {
+                        applyChan <- Message{ACK, id, Log{}, Log{}}
+                    }
+                default:
+                    chanIsEmpty = true
+            }
+        }
+        time.Sleep(1e6 * 100)
+    }
+
+    time.Sleep(1e6 * 100)
 }
 
 
 func RunFollower(peers []chan Message, id int, persister *Persister) {
-	lastHB := time.Now()
-	for time.Since(lastHB).Milliseconds() < leaderTimeOut {
-		select {
-			case message := <- peers[id]:
-				if message.messageType == HB {
-					lastHB = time.Now()
-				}
-			default:
-				time.Sleep(10 * 1e6)
-		}
-	}
+    lastHB := time.Now()
+    timeOut := int64(rand.Intn(maxTimeOut- minTimeOut) + minTimeOut)
+    
+    for time.Since(lastHB).Milliseconds() < timeOut {
+        select {
+            case message := <- peers[id]:
+                if message.messageType == HB {
+                    lastHB = time.Now()
+                }
+            default:
+                time.Sleep(10 * 1e6)
+        }
+    }
 }
 
 func RaftNode(peers []chan Message, id int, persister *Persister, applyChan chan Message) {
-	term := 0
-	//loop for terms
-	for term < 10 {
-		isLeader := ElectLeader(peers, id, persister)
-		if isLeader {
-			fmt.Print("term ")
-			fmt.Println(term)
-			fmt.Println(id)
-			RunLeader(peers, id, persister, applyChan)
-		} else {
-			RunFollower(peers, id, persister)
-		}
-		term++
-	}
-	
+    term := 0
+    
+    //loop for terms
+    for term < 10 {
+        isLeader := ElectLeader(peers, id, persister)
+        
+        if isLeader {
+            fmt.Print("term ")
+            fmt.Println(term)
+            fmt.Println(id)
+            RunLeader(peers, id, persister, applyChan)
+        } else {
+            RunFollower(peers, id, persister)
+        }
+        
+        term++
+    }
+    
 }
 
 func GetCurrentLeader(peers []chan Message, applyChan chan Message) int {
-	leader := -1
-	for i := 0; i < numNodes; i++ {
-		peers[i] <- Message{LEADER, -1}
-	}
-	seacrhStart := time.Now()
-	for leader < 0 && time.Since(seacrhStart).Milliseconds() < leaderTimeOut {
-		select {
-			case message := <- applyChan:
-				if message.messageType == ACK {
-					leader = message.sender
-				}
-			default:
-				time.Sleep(10 * 1e6)
-		}
-	}
-	return leader
+    leader := -1
+    
+    for i := 0; i < numNodes; i++ {
+        peers[i] <- Message{LEADER, -1, Log{}, Log{}}
+    }
+    
+    seacrhStart := time.Now()
+    
+    for leader < 0 && time.Since(seacrhStart).Milliseconds() < leaderTimeOut {
+        select {
+            case message := <- applyChan:
+                if message.messageType == ACK {
+                    leader = message.sender
+                }
+            default:
+                time.Sleep(10 * 1e6)
+        }
+    }
+    
+    return leader
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	peers := make([]chan Message, numNodes)
-	applyChan := make(chan Message, maxChanBuff)
-	persister := MakePersister()
-	for i := 0; i < numNodes; i++ {
-		peers[i] = make(chan Message, maxChanBuff)
-	}
+    rand.Seed(time.Now().UnixNano())
+    
+    peers := make([]chan Message, numNodes)
+    applyChan := make(chan Message, maxChanBuff)
+    
+    for i := 0; i < numNodes; i++ {
+        peers[i] = make(chan Message, maxChanBuff)
+    }
 
-	for i := 0; i < numNodes; i++ {
-		go RaftNode(peers, i, persister, applyChan)
-	}
-	/*
-	// test for get current leader functionality
-	time.Sleep(5e9)
-	fmt.Print("Current Leader: ")
-	fmt.Println(GetCurrentLeader(peers, applyChan))
-	*/
-	time.Sleep(15e9)
-	
+    for i := 0; i < numNodes; i++ {
+        persister := MakePersister()
+        go RaftNode(peers, i, persister, applyChan)
+    }
+    
+    /*
+    // test for get current leader functionality
+    time.Sleep(5e9)
+    fmt.Print("Current Leader: ")
+    fmt.Println(GetCurrentLeader(peers, applyChan))
+    */
+    
+    time.Sleep(30e9)
 }
