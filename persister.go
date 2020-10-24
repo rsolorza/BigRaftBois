afaa7d412d6b39d5650c2c289d9c533bfa3640d1
Ryan Solorzano <rsolorza@calpoly.edu>
2020-10-23 20:20:47 -0700

    Done writing persister.go
    Need to actually put it in Raft.go now

--- persister.go
+++ persister.go
@@ -14,91 +14,129 @@ type StateMachine struct {
 }
 
 type Persister struct {
-	currentTerm int
-	votedFor    int
-	logs        []Log
+    currentTerm int
+    votedFor    int
+    logs        []Log
 }
 
 type Follower struct {
-	commitIndex int
-	lastApplied int
+    commitIndex int
+    lastApplied int
 }
 
 type Leader struct {
-	nextIndex []int
-	matchIndex []int
+    nextIndex []int
+    matchIndex []int
 }
 
 func MakePersister() *Persister {
-	return &Persister{-1, -1, nil}
+    return &Persister{-1, -1, nil}
 }
 
 func (ps *Persister) MakeLeader(me *Follower, id int) *Leader {
-	leader := Leader{}
-	leader.nextIndex = make([]int, numNodes)
-	leader.matchIndex = make([]int, numNodes)
+    leader := Leader{}
+    leader.nextIndex = make([]int, numNodes)
+    leader.matchIndex = make([]int, numNodes)
 
-	for i := 0; i < id; i++ {
-		if i != id {
-			leader.nextIndex[i] = me.commitIndex + 1
-			leader.matchIndex[i] = 0
-		}
-	}
+    for i := 0; i < id; i++ {
+        if i != id {
+            leader.nextIndex[i] = me.commitIndex + 1
+            leader.matchIndex[i] = 0
+        }
+    }
 
-	return &leader
+    return &leader
 }
 
 // Can be used as initialization or recovery
 func (ps *Persister) MakeFollower() *Follower {
-	if (ps.logs == nil || ps.currentTerm == -1) {
-		ps.currentTerm = 0
-		ps.logs = make([]Log, 1)
+    if (ps.logs == nil || ps.currentTerm == -1) {
+        ps.currentTerm = 0
+        ps.logs = make([]Log, 1)
 
-		ps.logs[0] = Log{0, 0, false, StateMachine{0,0,0}}
+        ps.logs[0] = Log{0, 0, true, StateMachine{0,0,0}}
 
-		return &Follower{0, 0}
-	}
+        return &Follower{0, 0}
+    }
 
-	for i := len(ps.logs); i > 0; i-- {
-		if (ps.logs[i].committed) {
-			return &Follower{i, len(ps.logs)}
-		}
-	}
+    for i := len(ps.logs); i > 0; i-- {
+        if (ps.logs[i].committed) {
+            return &Follower{i, len(ps.logs)}
+        }
+    }
 
-	return &Follower{0, 0}
+    return &Follower{0, 0}
 }
 
 // returns true if able to add to log, false otherwise
+// What happens is recieve dup?
 func (ps *Persister) FollowerRecieveLog(me *Follower, log Log, prev Log) bool {
-	if prev.idx == ps.logs[me.lastApplied].idx && prev.term == ps.logs[me.lastApplied].idx {
-		ps.logs = append(ps.logs, log)
-		ps.logs[me.lastApplied].committed = true
-		
-		me.commitIndex++
-		me.lastApplied++
+    if prev.idx == ps.logs[me.lastApplied].idx && prev.term == ps.logs[me.lastApplied].idx {
+        ps.logs = append(ps.logs, log)
+        ps.logs[me.lastApplied].committed = true
+        
+        me.commitIndex++
+        me.lastApplied++
 
-		return true
-	}
+        return true
+    }
 
-	return false
+    if prev.idx <= ps.logs[me.lastApplied].idx && prev.term == ps.logs[me.lastApplied].idx {
+        return true
+    }
+
+    return false
 }
 
 // Assumes new log to be sent has been added, but not committed locally
 func (ps *Persister) SendLogs(me *Leader, peers []chan Message, id int) {
-	for i := 0; i < numNodes; i++ {
-		if (i != id) {
-			logIdx := me.nextIndex[i]
+    for i := 0; i < numNodes; i++ {
+        if (i != id) {
+            logIdx := me.nextIndex[i]
 
-			peers[i] <- Message{LOG, id, ps.logs[logIdx], ps.logs[logIdx - 1]}
-		}
-	}
+            peers[i] <- Message{LOG, id, ps.logs[logIdx], ps.logs[logIdx - 1]}
+        }
+    }
 }
 
 func (ps *Persister) LeaderRecieveLogAck(me *Leader, msg Message) {
-	
+    me.nextIndex[msg.sender]++
+    me.matchIndex[msg.sender] = msg.newLog.idx
 }
 
-func (ps *Persister) UpdateCommitted() {
+func (ps *Persister) UpdateLeaderLogCommits(me *Leader, id int) {
+    prevLastCommitted := -1
+    newLastCommitted := -1
 
+    for i := len(ps.logs); i >= 0 && prevLastCommitted < 0; i-- {
+        if ps.logs[i].committed {
+            prevLastCommitted = i
+            newLastCommitted = i
+        }
+    }
+
+    // Go through and check each index of committed logs
+    for i := lastCommitted + 1; i < len(ps.logs) && prevLastCommitted == newLastCommitted; i++ {
+        majorityCommitted := false
+        commitCounter := 0
+
+        for j := 0; j < numNodes && !majorityCommitted; j++ {
+            nodeLogIndex := me.nextIndex[j]
+
+            if nodeLogIndex >= i {
+                commitCounter++
+            }
+
+            // simple majority (count self)
+            majorityCommitted = commitCounter > (numNodes/2)
+        }
+
+        if !majorityCommitted {
+            newLastCommitted = i
+        }
+    }
+
+    for i := prevLastCommitted; i <= newLastCommitted; i++ {
+        ps.logs[i].committed = true
+    }
 }
-
